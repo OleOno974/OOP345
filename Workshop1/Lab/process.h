7f8dbb707243d06b45fa7b6cfb84bc1316ef6e1c
Olena Onopriyenko <54893280+OleOno974@users.noreply.github.com>
2020-01-04 16:33:09 -0500

    Update process.h

--- Workshop1/Lab/process.h
+++ Workshop1/Lab/process.h
@@ -1,1 +1,15 @@
-
+/*= ================================================================ =
+	Name: Olena Onopriyenko
+	File : process.h
+	Email : oonopriyenko@myseneca.ca
+	Student Number : 124605163
+	Course Code : OOP345
+	Date : 9/13/2019
+	Workshop: Workshop1 / Lab
+	I confirm that the content of this file is created by me,
+	with the exception of the parts provided to me by my professor.
+	==================================================================*/
+namespace sdds
+{
+	void process(const char* str);
+}
